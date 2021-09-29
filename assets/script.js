ad23e123e94fb5451264c3bb7ff7eea7f8140a75
ArgenX1 <djc.arizona@gmail.com>
2021-09-29 16:34:37 -0700

    finised javaScript for app to run as intended

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
-const timeBlock = $(".time-block")
+const timeBlock = $(".time-block");
+var savedText = {}
 
 function initialize() {
-    setInterval(() => $("#currentDay").text(moment().format("LLLL")),1000)
+    setInterval(() => $("#currentDay").text(moment().format("LLLL")),1000);
+    savedText = JSON.parse(localStorage.getItem("savedText"));
     timeBlock.each(function() {
-        if(moment().format("H") > $(this).data("hour"))$(this).children("textarea").addClass("past")
-        else if(moment().format("H") == $(this).data("hour"))$(this).children("textarea").addClass("present")
-        else $(this).children("textarea").addClass("future")
-    })
-} 
-initialize()
+        if(moment().format("H") > $(this).data("hour"))$(this).children("textarea").addClass("past");
+        else if(moment().format("H") == $(this).data("hour"))$(this).children("textarea").addClass("present");
+        else $(this).children("textarea").addClass("future");
+        ($(this).children("textarea").text(savedText[$(this).data("hour")]));
+    });
+}
+initialize();
 
+$(".saveBtn").click(function(){
+    savedText[$(this).parent().data("hour")] = $(this).siblings("textarea").val();
+    localStorage.setItem("savedText", JSON.stringify(savedText));
+});
